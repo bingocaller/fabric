11dbbcfc4fd2ed9c92b1d5edc87b0558bd528ae6
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2022-02-01 13:10:22 +0100

    Use tokens, show tokens under Design
    Import token values from Figma Tokens and transform them using Style
    Dictionary.
    Use tokens in relevant places (mostly for spacing).
    Add Design Tokens page to show the generated values as cards.

---
+++ src/tokens.ts
@@ -0,0 +1,21 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 21 Jan 2022 22:53:28 GMT
+ */
+
+export const sizeSpacing3xs = "0.125rem";
+export const sizeSpacing2xs = "0.25rem";
+export const sizeSpacingXs = "0.5rem";
+export const sizeSpacingSm = "0.75rem";
+export const sizeSpacingMd = "1rem";
+export const sizeSpacingLg = "1.25rem";
+export const sizeSpacingXl = "1.5rem";
+export const sizeSpacing2xl = "1.75rem";
+export const sizeSpacing3xl = "2rem";
+export const sizeBorderRadiusSmall = "0.25rem";
+export const sizeBorderRadiusMedium = "0.5rem";
+export const sizeBorderRadiusLarge = "1rem";
+export const sizeBorderWidthHairline = "0.0625rem";
+export const sizeBorderWidthThin = "0.125rem";
+export const sizeBorderWidthThick = "0.25rem";
+export const colorBackgroundOverlayDefault = "rgba(0, 0, 0, 0.3)";
